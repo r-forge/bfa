f5e1be6692f460ef271ce44d4a4bf629c4e64ccf
jmurray1022 (Jared Murray) <jared.murray@stat.duke.edu>
2011-10-29 22:54:31 +0000

    Update documentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfa@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ratings08.R
+++ pkg/R/ratings08.R
@@ -1,6 +1,6 @@
 #' Special Interest Group Ratings (2008)
 #' 
-#' This dataset consists of SIG ratings of members of the US House and Senate.
+#' This dataset consists of special interest group ratings of members of the US House and Senate.
 #' It contains two objects: \code{ratings}, a data frame with the ratings
 #' information, and \code{groups} which contains information about the
 #' special interest groups
@@ -8,7 +8,7 @@
 #' @usage ratings08
 #' @format List of two data frames: \code{ratings} and \code{groups} (see above)
 #' @docType data
-#' @source Scraped from Project VoteSmart: \url{http://www.projectvotesmart.org}
+#' @source Accessed through the Project VoteSmart API: \url{http://www.projectvotesmart.org}
 #' @author Jared Murray \email{jared.murray@@stat.duke.edu}
 # @references 
 # \url{url}
